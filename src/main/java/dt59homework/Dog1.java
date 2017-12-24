af379fe60e675decb97244a63f37b309c94abe4d
GUO <GUO@DESKTOP-1AEO5CL>
2017-12-24 18:54:01 +0800

    20171224

---
+++ src/main/java/dt59homework/Dog1.java
@@ -0,0 +1,39 @@
+/**
+ * Project Name:dt59homework
+ * File Name:Dog1.java
+ * Package Name:dt59homework
+ * Date:2017年12月24日下午6:49:43
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 下午6:49:43 <br/>
+ * 
+ * @author GUO
+ * @version
+ * @see
+ */
+public class Dog1 {
+
+    String name;
+
+    String color;
+
+    int age;
+
+    void eat() {
+        System.out.println("爱吃狗粮");
+    }
+
+    void destroy() {
+        System.out.println("喜欢破坏家具");
+    }
+
+    void walk() {
+        System.out.println("喜欢散步");
+    }
+
+}
